9027bbeb9b9d5d4326e97f207bace489f77dc926
neilge <neilge@audible.com>
2017-09-15 16:46:30 -0400

    bean life cycle with annotation

--- src/com/neilge/demo/TennisCoach.java
+++ src/com/neilge/demo/TennisCoach.java
@@ -2,20 +2,31 @@ package com.neilge.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 /**
  * @author neilge
  * @since 2017-09-15
  */
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach {
 
     @Autowired
     @Qualifier("randomFortuneService")
     private FortuneService fortuneService;
+
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println(">> TennisCoach: inside of doMyStartupStuff");
+    }
+
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println(">> TennisCoach: inside of doMyCleanupStuff");
+    }
 
     @Override
     public String getDailyWorkout() {
@@ -26,6 +37,4 @@ public class TennisCoach implements Coach {
     public String getDailyFortune() {
         return fortuneService.getDailyFortune();
     }
-
-
 }
