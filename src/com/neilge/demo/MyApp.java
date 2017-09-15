1eb20d52ca942d4976b1acef50e3fa55751f963d
NilGE <gxy26904@gmail.com>
2017-09-15 08:47:09 -0400

    Add gitignore file

--- src/com/neilge/demo/MyApp.java
+++ src/com/neilge/demo/MyApp.java
@@ -6,7 +6,7 @@ package com.neilge.demo;
 public class MyApp {
 
     public static void main(String[] args) {
-        
+
         Coach theCoach = new TrackCoach();
         System.out.println(theCoach.getDailyWorkout());
     }
